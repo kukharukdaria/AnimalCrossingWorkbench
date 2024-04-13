58346b0e20142aaa2b735fbe8686235c5ece0b02
kukharukdaria <kukharukdaria@gmail.com>
2024-04-12 22:47:54 -0600

    r

--- workbench/src/pages/AxePage.js
+++ workbench/src/pages/AxePage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import FlimsyAxeCard from '../JScomponents/FlimsyAxeCard.js'
 
 function AxePage() {
   const navigate = useNavigate();
   return (
-    <div>
+    <div style={{
+      align: 'center'
+    }}>
+      <div style={{ display: 'inline-block', textAlign: 'center' }}> {/* Center align the FlimsyAxeCard */}
+        <FlimsyAxeCard/>
+      </div>
       <h1>Axes are sharp</h1>
       <button onClick={() => {
             navigate("/AnimalCrossingWorkbench/");
